cfabd70ba663a05057bce6359d7f6e5065eec201
Ty Overby <ty@pre-alpha.com>
2022-01-17 17:01:08 -0500

    _

--- import-scripts/async.sh
+++ import-scripts/async.sh
@@ -4,6 +4,7 @@ source "$(git rev-parse --show-toplevel)/import-scripts/util.sh"
 get janestreet textutils master
 get janestreet async_kernel master
 get janestreet async_rpc_kernel master
+get janestreet async_rpc_websocket master
 get janestreet async_unix master
 get janestreet async master
 get janestreet async_js master
