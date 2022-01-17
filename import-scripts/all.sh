5a021db022bd0df66b3ef97449c1938252fc52a2
Ty Overby <ty@pre-alpha.com>
2022-01-17 15:52:11 -0500

    _

--- import-scripts/all.sh
+++ import-scripts/all.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-"$SCRIPT_DIR/bonsai.sh"
-"$SCRIPT_DIR/incremental.sh"
+"$SCRIPT_DIR/bonsai.sh" & 
+"$SCRIPT_DIR/incremental.sh" &
+"$SCRIPT_DIR/core_unix.sh" &
+
+wait
 
