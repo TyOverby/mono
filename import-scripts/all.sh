cf7ca6fcaef4d506219c85619da4f230f7c35416
Ty Overby <ty@pre-alpha.com>
2022-01-17 14:27:39 -0500

    _

---
+++ import-scripts/all.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+./bonsai.sh 
+./incremental.sh
+
--- import-scripts/generate-patches.sh
+++ import-scripts/generate-patches.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
+set -euo pipefail 
 
 export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-VENDOR_DIR="$SCRIPT_DIR/../vendor"
+PROJECT_DIR="$SCRIPT_DIR/../"
+VENDOR_DIR="$PROJECT_DIR/vendor"
+PATCHES_DIR="$PROJECT_DIR/_patches"
+
+function rev_with_content() {
+  git add -A  > /dev/null
+  git commit -m "_" > /dev/null
+  git rev-parse HEAD
+}
+
+AFTER=$(rev_with_content)
+
+"$VENDOR_DIR/all.sh"
+BEFORE=$(rev_with_content)
+
+
 
 for dir in $(ls "$VENDOR_DIR" | grep "-"); do 
-  cd "$dir"
-  git diff .
+  cd "$VENDOR_DIR/$dir"
+  git diff "$BEFORE" "$AFTER" .
 done
