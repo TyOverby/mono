954c935d7f68b154f711663436b1f81b26da8b9c
Ty Overby <ty@pre-alpha.com>
2022-01-29 13:27:17 -0500

    _

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -9,35 +9,34 @@ VER="4.12.1"
 JS_BLEED_VER="cf2164a995a0d0577cd54beef5d2471e1a02f61f"
 
 function make_switch() {
-    root=$1
+    root="$1"
     mkdir -p "$root"
+    cd "$root"
 
-    if [ -d "$root/_opam" ]; then
-        if cat "$root/_opam/.opam-switch/switch-config" | grep "$VER" > /dev/null; then 
+    if [ -d "./_opam" ]; then
+        if cat "./_opam/.opam-switch/switch-config" | grep "$VER" > /dev/null; then 
             echo "opam switch for $VER already created"
         else 
             echo "old switch detected, removing..."
-            rm -rf "$root/_opam"
+            rm -rf "./_opam"
         fi
     fi 
 
-    if [ -d "$root/_opam" ]; then
+    if [ -d "./_opam" ]; then
         true
     else 
-        opam switch create "$root" "ocaml-base-compiler.$VER" --no-install
+        opam switch create "./" "ocaml-base-compiler.$VER" --no-install
     fi
 }
 
 (
   make_switch "$TOOLS_DIR"
-  cd "$TOOLS_DIR"
   eval $(opam env)
   opam install -y ocamlformat ocamlformat-rpc ocaml-lsp-server
 ) & 
 
 (
   make_switch "$PROJECT_DIR"
-  cd "$PROJECT_DIR"
   eval $(opam env)
   opam repo add "janestreet-bleeding-$JS_BLEED_VER" "https://github.com/janestreet/opam-repository.git#cf2164a995a0d0577cd54beef5d2471e1a02f61f"
   opam install -y dune re js_of_ocaml js_of_ocaml-ppx gen_js_api pcre lambdasoup sedlex fmt cryptokit ctypes ctypes-foreign ctypes-build angstrom stringext
