ab9c8ad4f389a031dea2dad6f80f7c10e8c443ff
Ty Overby <ty@pre-alpha.com>
2022-01-29 14:18:03 -0500

    _

--- import-scripts/bonsai.sh
+++ import-scripts/bonsai.sh
@@ -4,10 +4,12 @@ source "$(git rev-parse --show-toplevel)/import-scripts/util.sh"
 get janestreet ocaml-embed-file master
 
 get janestreet incr_dom master
+apply_patches janestreet incr_dom
 rm -rf example
 
 get janestreet virtual_dom master
 rm -rf tyxml 
 
 get janestreet bonsai master
+apply_patches janestreet bonsai
 rm -rf experimental
