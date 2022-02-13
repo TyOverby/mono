50698864d94dac5871cd6bdaf2663d45c0ba8df0
Ty Overby <ty@pre-alpha.com>
2022-02-13 13:18:38 -0500

    _

--- scripts/copy_for_deployment.sh
+++ scripts/copy_for_deployment.sh
@@ -12,3 +12,9 @@ rsync \
    --exclude="*" \
    ./_build/default/app/ \
    ./_public
+
+find _public \
+  | grep index.html \
+  | sed 's:_public/\(\(.*\)/index.html\):<a href="\1"> \2 </a>:' \
+  > _public/index.html
+
